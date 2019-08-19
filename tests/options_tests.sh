70b302367b3ae3b953b62fd881f6adfd9c42c9b7
Azadeh Bagheri <Azadeh.Bagheri@loblaw.ca>
2019-08-19 10:35:03 -0400

    Changed gitrise.sh to make it testable and added some tests Changed the way environment variables are passed to gitrise; we now use ':' to separate key-value pairs(key:value) passed with option -e to be able to pass strings including '=' To reflect the above change, modified the usage function in gitrise.sh and the README Created the tests directory and added 2 tests in the build_tests.sh file and 8 tests in the options_tests.sh file Created travis.yml file and added the tests to the pipeline

---
+++ tests/options_tests.sh
@@ -0,0 +1,55 @@
+#! /bin/bash
+
+# shellcheck disable=SC1091
+# shellcheck disable=SC2006
+source ./tests/test_helper.sh
+
+testEnvVars() {
+    local expected="[{\"mapped_to\":\"CODE_COVERAGE\",\"value\":\"true\",\"is_expand\":true},{\"mapped_to\":\"ENVIRONMENT\",\"value\":\"UAT\",\"is_expand\":true}]"
+    local actual=$(process_env_vars "CODE_COVERAGE:true,ENVIRONMENT:UAT")
+    assertEquals "$expected" "$actual"
+}
+
+testpassingURLAsEnvVar() {
+    local expected="[{\"mapped_to\":\"CODE_COVERAGE\",\"value\":\"http://test.com\",\"is_expand\":true}]"
+    local actual=$(process_env_vars "CODE_COVERAGE:http\://test.com")
+    assertEquals "$expected" "$actual"
+}
+
+testPassingFileAsEnvVar() {
+    local expected="[{\"mapped_to\":\"test_key\",\"value\":\"TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4==\",\"is_expand\":true}]"
+    local actual=$(process_env_vars "test_key:$(<./testdata/test_env_file.txt)")  
+    assertEquals "$expected" "$actual"
+}
+
+testHelp(){
+    local expected=$(usage)
+    local actual=$(./gitrise.sh -h)
+    assertEquals "$expected" "$actual"
+}
+
+testVersion(){
+    local expected_version="$VERSION"
+    local result=$(./gitrise.sh -v)
+    local actual_version=$(echo "$result" | grep -o '[0-9]\{1,\}.[0-9]\{1,\}.[0-9]\{1,\}')
+    assertEquals "${expected_version}" "${actual_version}"
+}
+
+testTestingModeOption() {
+    assertTrue "TESTING_ENABLED is not set to 'true'" "${TESTING_ENABLED}"
+}
+
+testBranchOption(){
+    local expected_branch="test_branch"
+    source ./gitrise.sh -t -b test_branch
+    local actual_branch="$BRANCH"
+    assertEquals "${expected_branch}" "${actual_branch}"
+}
+
+testWrongUsage(){
+    local expected=$(printf "Invalid option '-w,'\n%s" "$(usage)")
+    local result=$(./gitrise.sh -t -w, test_workflow)
+    assertEquals "$expected" "$result"
+}
+ 
+. ./tests/shunit2
