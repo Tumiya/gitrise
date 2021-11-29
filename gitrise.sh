a73e8e839a037e8430687e986756020a640fc44c
THAVEAU Alexis <alexis.thaveau.ext@s2e-net.com>
2021-11-29 09:34:46 +0100

    (fix) Ensure download dir exist

--- gitrise.sh
+++ gitrise.sh
@@ -317,8 +317,10 @@ function get_build_logs() {
 download_artifacts() {
   if [ "$DOWNLOAD_ARTIFACTS" = "true" ]; then
     OUTPUT_DIR=${DOWNLOAD_DIR:-'./'}
+    # Ensute OUTPUT_DIR exist
+    mkdir -p $OUTPUT_DIR
     echo "downloading artifacts to $OUTPUT_DIR"
-    # Récuperation du artifact slug
+    # Get artifacts slug
     local command="curl --silent -X GET https://api.bitrise.io/v0.1/apps/$PROJECT_SLUG/builds/$build_slug/artifacts \
                   --header 'Accept: application/json' --header 'Authorization: $ACCESS_TOKEN'"
     response=$(eval "$command")
