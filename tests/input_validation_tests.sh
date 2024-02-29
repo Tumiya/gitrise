fe8bff2a665e41d0eba72202200105febba86949
Azadeh Bagheri <azadeh.bagheri7@gmail.com>
2024-02-29 14:53:55 -0500

    Fixed the bug with the 'ERROR: Missing build artifact name' showing all the time

--- tests/input_validation_tests.sh
+++ tests/input_validation_tests.sh
@@ -57,10 +57,4 @@ testAcceptablePollingInterval() {
     local actual_message=$(./gitrise.sh -s "test-slug" -a "test-token" -w "test-workflow" -b "test-branch" -p 10)
     assertNotContains "Output message does not match." "${actual_message}" "${not_expected_message}"
 }
-
-testEmptyBuildArtifactName() {
-    local expected_message="ERROR: Missing build artifact name."
-    local actual_message=$(./gitrise.sh -s "test-slug" -a "test-token" -w "test-workflow" -b "test-branch" --download-artifacts)
-    assertContains "Output message does not match." "${actual_message}" "${expected_message}"
-}
 . ./tests/shunit2
